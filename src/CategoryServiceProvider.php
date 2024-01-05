5d3e4e2e5589a07ca16c04cbc696b816c1bb079b
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-05 23:36:25 +0330

    init package

---
+++ src/CategoryServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace JobMetric\Category;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class CategoryServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws MigrationFolderNotFoundException
+     * @throws RegisterClassTypeNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-category')
+            ->hasConfig()
+            ->hasMigration()
+            ->registerClass('Category', Category::class);
+    }
+}
