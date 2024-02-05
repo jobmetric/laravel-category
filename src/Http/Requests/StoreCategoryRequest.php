3656eb82ea862a4f83bdacd7a0bd05bcc62ae7f5
majid mohammadian <majeedmohammadian@gmail.com>
2024-02-05 03:45:38 +0330

    add request for store category

---
+++ src/Http/Requests/StoreCategoryRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace JobMetric\Category\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'slug' => 'nullable|string',
+            'parent_id' => 'integer',
+            'type' => 'string',
+            'ordering' => 'integer',
+            'status' => 'boolean',
+
+            'translations' => 'required|array',
+            'translations.*.title' => 'required|string',
+            'translations.*.body' => 'string|nullable',
+            'translations.*.meta_title' => 'string|nullable',
+            'translations.*.meta_description' => 'string|nullable',
+            'translations.*.meta_keywords' => 'string|nullable',
+        ];
+    }
+}
