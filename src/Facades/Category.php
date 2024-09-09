782b142d06092d83f8d8ab44a7667d685bbe2e92
majidmohammadian <majeedmohammadian@gmail.com>
2024-09-09 19:27:39 +0330

    complete facade method

--- src/Facades/Category.php
+++ src/Facades/Category.php
@@ -7,11 +7,13 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \JobMetric\Category\Category
  *
+ * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection paginate(string $type, array $filter = [], int $page_limit = 15, array $with = [])
+ * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection all(string $type, array $filter = [], array $with = [])
  * @method static array store(array $data)
  * @method static array update(int $category_id, array $data)
  * @method static array delete(int $category_id)
  * @method static string getName(int $category_id, bool $concat = true, string $locale = null)
- * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection usedIn(int $category_id)
+ * @method static array usedIn(int $category_id)
  * @method static bool hasUsed(int $category_id)
  */
 class Category extends Facade
