7ed2fd3d996057d75967fd2e9832735fe11cfcf6
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-24 00:23:21 +0330

    added base controller

---
+++ src/Http/Controllers/Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JobMetric\Category\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use JobMetric\PackageCore\Controllers\HasResponse;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests, HasResponse;
+}
