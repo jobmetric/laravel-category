e399b5af8cf4e414cc4e41d407e4dad0950370b3
majidmohammadian <majeedmohammadian@gmail.com>
2024-11-05 19:51:24 +0330

    change version url package to 1.5 for add component url slug

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -25,22 +25,7 @@
             @endif
 
             @if($has_url)
-                <!--begin::Slug-->
-                <div class="card card-flush py-4">
-                    <div class="card-header">
-                        <div class="card-title">
-                            <span class="fs-5 fw-bold">{{ trans('url::base.fields.slug.title') }}</span>
-                        </div>
-                    </div>
-                    <div class="card-body pt-0">
-                        <input type="text" name="slug" class="form-control" placeholder="{{ trans('url::base.fields.slug.placeholder') }}" value="{{ old('slug') }}">
-                        @error('slug')
-                            <div class="form-errors text-danger fs-7 mt-2">{{ $message }}</div>
-                        @enderror
-                        <div class="mt-5 text-gray-600 fs-7">{{ trans('url::base.fields.slug.description') }}</div>
-                    </div>
-                </div>
-                <!--end::Slug-->
+                <x-url-slug value="{{ old('slug') }}" />
             @endif
 
             <!--begin::Status-->
